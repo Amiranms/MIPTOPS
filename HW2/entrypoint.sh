67bdcf54d1e72ac707bc162a8660aff8bff894c9
amiranms <amiran.mst@yandex.ru>
2025-04-14 12:28:28 +0300

    env filest use

--- HW2/entrypoint.sh
+++ HW2/entrypoint.sh
@@ -3,12 +3,17 @@ set -e
 
 envsubst < /app/listmonk/config.tmpl > /app/listmonk/config.toml
 
-while ! nc -z postgres 5432; do
+while ! nc -z postgres $DB_PORT; do
   sleep 2
 done
 
+if [ -n "$PERMISSIONS_JSON" ]; then
+    echo "$PERMISSIONS_JSON" > /app/listmonk/permissions.json
+fi
+
+
 cd /app/listmonk 
 
-echo "y" | /app/listmonk/listmonk --install
+echo "y" | /app/listmonk/listmonk --upgrade
 
 exec /app/listmonk/listmonk
